ac2d73cc4ccdc8fbb76696ffdc9ee57b29e3726a
t97eb <iraqi1234baldawi12352@gmail.com>
2025-08-15 03:11:19 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,12 @@
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Cortex AI Bot is running!"
+
+def keep_alive():
+    t = Thread(target=app.run, args=("0.0.0.0", 8080))
+    t.start()
